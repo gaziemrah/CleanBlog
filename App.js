d10f15f2a400fa5078340f8b4bbe2943bf27e78e
unknown <g.emrahartac@gmail.com>
2021-12-06 20:03:10 +0300

    git

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    const blog = {
-        id:1,
-        title:'Blog',
-        description: 'Blog Description'
-    }
+app.get('/', (req, res) => {
+  const blog = {
+    id: 1,
+    title: 'Blog',
+    description: 'Blog Description',
+  };
 
-    res.send(blog);
-})
+  res.send(blog);
+});
 
 const port = 3000;
-app.listen(port, ()=>{
-    console.log(`Server ${port}'dan dinleniyor'`);
-})
+app.listen(port, () => {
+  console.log(`Server ${port}'dan dinleniyor'`);
+});
